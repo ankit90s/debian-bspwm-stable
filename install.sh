3340bbf29501ddc6d320a16d62d68535cbf0c035
ankit90s <ak7591527@gmail.com>
2022-12-09 18:03:03 +0530

    added fzf

--- install.sh
+++ install.sh
@@ -96,7 +96,7 @@ sudo apt install -y espeak translate-shell cmatrix hddtemp ncdu ranger calcurse
 # sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 
 # Additional Packages
-sudo apt install -y xsel psmisc grep sed gawk openssl ffmpeg qalc gzip
+sudo apt install -y xsel psmisc grep sed gawk openssl ffmpeg qalc gzip fzf zip
 
 # Redshift installation
 sudo apt install -y redshift
